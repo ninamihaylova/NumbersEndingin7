4cd5cae107d0ee8ef9c8f827487cd529ae2c5d01
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 20:53:38 +0300

    Initial commit

---
+++ NumbersEndingin7/Program.cs
@@ -0,0 +1,16 @@
+﻿class NumbersEndingin7
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        for (int i = 7; i <= n; i++)
+        {
+            if (i % 10 == 7)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
